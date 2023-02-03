6e9e86cf4fa7e09663a6c8f05a67c0ee366e0e88
Atara02 <yeojp73@gmail.com>
2023-02-04 03:25:55 +0900

    에너미 이동

--- Assets/Atara/Enemy/EnemyMemoryPool.cs
+++ Assets/Atara/Enemy/EnemyMemoryPool.cs
@@ -8,18 +8,20 @@ public class EnemyMemoryPool : MonoBehaviour
     private GameObject enemyPrefab;   //积己登绰 利
 
     [SerializeField]
-    private float enemySpawnTime = 1;   //利 积己 林扁
+    private float enemySpawnTime = 5f;   //利 积己 林扁
 
     private MemoryPool enemyMemoryPool;   //利 力绢侩
 
-    private int numberOfEnemiesSpawnedAtOnce = 1;  //悼矫俊 积己登绰 利狼 荐
-
     [SerializeField]
     private Transform SpawnPoint;
+    [SerializeField]
+    private Transform target;
+    private Vector3 direction;
 
     private void Awake()
     {
         enemyMemoryPool = new MemoryPool(enemyPrefab);
+        direction = (target.position - this.transform.position).normalized;
 
         StartCoroutine("SpawnEnemy");
     }
@@ -31,7 +33,14 @@ public class EnemyMemoryPool : MonoBehaviour
             GameObject item = enemyMemoryPool.ActivePoolItem();
             item.transform.position = SpawnPoint.position;
 
+            item.GetComponent<EnemyMovement>().SetUp(this, target, direction);
+
             yield return new WaitForSeconds(enemySpawnTime);
         }
+    }
+
+    public void DeactiveEnemy(GameObject enemy)
+    {
+        enemyMemoryPool.DeactivePoolItem(enemy);
     }
 }
--- Assets/Atara/Enemy/EnemyMovement.cs
+++ Assets/Atara/Enemy/EnemyMovement.cs
@@ -4,9 +4,53 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    //적 메모리 풀
+    private EnemyMemoryPool enemyMemoryPool;
+    //움직일 방향
+    private Vector3 direction;
+    //나무의 위치
+    private Transform target;
+    //나무와 적 간의 거리
+    [SerializeField]
+    private float distance;
 
+    [Header("Status")]
+    [SerializeField]
+    private int maxHP = 10;
+    private int currentHP;
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+
+    public void SetUp(EnemyMemoryPool enemyMemoryPool, Transform target, Vector3 direction)
+    {
+        this.enemyMemoryPool = enemyMemoryPool;
+        this.target = target;
+        this.direction = direction;
+
+        currentHP = maxHP;
+    }
     private void Update()
     {
-        transform.position += Vector3.left * Time.deltaTime;
+        float dis = (target.position - this.transform.position).magnitude;
+        if(dis <= distance)
+        {
+            Debug.Log("Stop");
+        }
+        else
+        {
+            transform.position += direction * Time.deltaTime * moveSpeed;
+            //Vector3 direction = (target.position - this.transform.position).normalized;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if(currentHP <= 0)
+        {
+            enemyMemoryPool.DeactiveEnemy(this.gameObject);
+        }
     }
 }
