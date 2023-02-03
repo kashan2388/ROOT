15811ab64f0a4af60c945ab0ed0c79b062f2f461
kashan2388 <defalt730@gmail.com>
2023-02-04 03:27:04 +0900

    마우스 포지션

--- Assets/JK/Script/InputManager.cs
+++ Assets/JK/Script/InputManager.cs
@@ -1,16 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    void Start()
+    [NonSerialized] public Vector3 cameraPoint;
+    public void CameraPoint(Vector3 point)
     {
-        
+        // 카메라 위치
+        point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y, -Camera.main.transform.position.z));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Debug.Log(point.ToString());
+        }
     }
 
-    void Update()
+    private void Update()
     {
+        CameraPoint(cameraPoint);
+
         
+
     }
+
+
+
+
 }
