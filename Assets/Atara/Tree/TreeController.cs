bd213da8390e197973cf5ec00a6d9934355ff9de
kashan2388 <defalt730@gmail.com>
2023-02-04 12:09:39 +0900

    임시 죽순 소환

--- Assets/Atara/Tree/TreeController.cs
+++ Assets/Atara/Tree/TreeController.cs
@@ -1,21 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class TreeController : MonoBehaviour
 {
+    public GameObject bamboo;
+    [NonSerialized] public Vector3 cameraPoint;
     private void Update()
     {
-        OnClick();
+        OnClick(cameraPoint);
     }
-    private void OnClick()
+    private void OnClick(Vector3 point)
     {
         //클릭한 곳의 정보를 저장할 변수
         RaycastHit hit;
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log(point.ToString());
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y, -Camera.main.transform.position.z));
+
+            Instantiate(bamboo, new Vector3 (point.x, 0, 0), Quaternion.Euler(-90f, 0, 0));
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.transform.name);
@@ -28,8 +39,12 @@ public class TreeController : MonoBehaviour
                 else if (hit.transform.gameObject.CompareTag("Enemy"))
                 {
                     hit.transform.GetComponent<EnemyMovement>().TakeDamage(10);
+
                 }
             }
         }
+
+        
     }
+
 }
